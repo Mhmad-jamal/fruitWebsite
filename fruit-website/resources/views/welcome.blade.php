8394e20556896637da6d263029420d2db847ede6
mhmad-jamal <mohammad52008@hotmail.com>
2023-05-21 16:47:52 +0300

    update

--- fruit-website/resources/views/welcome.blade.php
+++ fruit-website/resources/views/welcome.blade.php
@@ -11,5 +11,63 @@
        <button id="orderbtn">order now </button>
     </div>
 </section>
+<section class="container mt-5 ">
+
+  <div class="row pt-3 pb-3">
+    <div class="col d-flex justify-content-center mb-3">
+        <div class="card text-center" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title text-center">Card title</h5>
+            <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button href="#" class="btn card-btn">Go somewhere</button>
+          </div>
+        </div>
+      </div>
+      
+
+
+
+</div>
+</section>
+<section class="container-fluid mt-3" id="section3" >
+    <div class="row justify-content-center " style="padding: 10% 0 ;">
+    
+<div class="col-4 d-flex justify-content-center  flex-column">
+<div class="d-flex justify-content-end  flex-column">    <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+    <p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+
+</div>
+<div class=" d-flex justify-content-end flex-column">    <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+    <p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+
+</div>
+
+<div class=" d-flex justify-content-end flex-column">    <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+    <p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+
+</div>
+
+</div>
+<div class="col-3 d-flex justify-content-center align-items-center "> 
+    <img  class="img-fluid max-width-100" style="height: fit-content" id="Center-img" src="{{asset('user/main-img/Fruit-Box.png')}}" alt="">
+
+
+</div>
+<div class="col-4 d-flex justify-content-center flex-column">
+    <div>
+        <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+
+    </div>
+    <div>    <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+    </div>
+    
+    <div>    <img  class="img-fluid w-50 mx-auto" id="" src="{{asset('user/main-img/Main_IMG.png')}}" alt="">
+    </div>
+</div>
+
+
+
+</section>
   
 @endsection
