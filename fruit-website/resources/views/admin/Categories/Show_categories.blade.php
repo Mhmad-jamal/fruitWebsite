402bb2d805e4b97585bc1046ea30ae0ff1300dd6
Mhmad-jamal <mohammed52008@hotmail.com>
2023-05-26 22:00:05 +0300

    update

--- fruit-website/resources/views/admin/Categories/Show_categories.blade.php
+++ fruit-website/resources/views/admin/Categories/Show_categories.blade.php
@@ -49,8 +49,8 @@
                           <i class="bx bx-dots-vertical-rounded"></i>
                         </button>
                         <div class="dropdown-menu">
-                          <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                          <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                          <a class="dropdown-item" data-id="{{$category->id}}" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                          <a class="dropdown-item" href="{{route('edit_Ca')}}" data-id="{{$category->id}}"><i class="bx bx-trash me-1"></i> Delete</a>
                         </div>
                       </div>
                     </td>
