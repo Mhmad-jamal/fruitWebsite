3536f6af0e0ef74db1f3bd7176de0dcf7ae6c845
zorz0 <83619017+zorz0@users.noreply.github.com>
2023-07-06 23:12:20 +0300

    blog code backend

---
+++ fruit-website/app/Models/BlogPost.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    protected $fillable = ['title', 'short_description', 'content', 'image'];
+
+    // Optional: If you want to specify a different table name
+    // protected $table = 'blog_posts';
+
+    // Optional: If you have relationships with other models
+    // public function comments()
+    // {
+    //     return $this->hasMany(Comment::class);
+    // }
+}
+
--- fruit-website/routes/web.php
+++ fruit-website/routes/web.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\PaymentController;
 
 
+use App\Http\Controllers\BlogPostController;
 
 
 /*
@@ -45,7 +46,7 @@ Route::get('/about', function () {
     $about = About::first(); // Retrieve the first About record from the database
 
     return view('user.about')->with('about',$about);
-    
+
 })->name('about');
 Route::get('/article/{id}', [Article::class, 'show'])->name('single-article');
 
@@ -91,7 +92,7 @@ Route::middleware(['CheckRole:admin'])->group(function () {
         Route::post('/categories', [CategoriesController::class, 'store'])->name('categories.store');
 
             Route::post('/category/update', [CategoriesController::class, 'update'])->name('categories.update');
-            
+
             Route::get('/categories/allCategories', [CategoriesController::class, 'allCategories'])->name('categories.allCategories');
             Route::get('/category/show/{id}', [CategoriesController::class, 'show'])->name('category.show');
             Route::get('/category/edit/{id}', [CategoriesController::class, 'edit'])->name('category.edit');
@@ -114,21 +115,21 @@ Route::any('/Product/all', function () {
     $products = Product::join('categories', 'categories.id', '=', 'products.category_id')
     ->select('products.*', 'categories.category_name as category_name')
     ->get();
-    
+
     return view('admin.Product.View_all')->with('products', $products);
 })->name('Product.View_all');
 
 // gift route
 Route::any('/Gift/all', function () {
     $Gifts = Gift::all();
- 
+
     return view('admin.Gift.View_all')->with('Gifts', $Gifts);
 })->name('Gift.View_all');
 
 
 Route::any('/paymentPage', function () {
-   
- 
+
+
     return view('user.visaPayment');
 })->name('visaPayment');
 Route::get('/Gift/view/{id}', [GiftController::class, 'view'])->name('gift.details');
@@ -161,7 +162,7 @@ Route::middleware(['CheckRole:' . 'admin' . '!' . 'user'])->group(function () {
 
 Route::any('/Payment', [PaymentController::class, 'view'])->name('payment.create');
 
-/// user login route 
+/// user login route
 Route::middleware('auth')->group(function () {
     // Protected routes for authenticated users
     Route::post('/cart/add', [CartController::class, 'create'])->name('cart.create');
@@ -174,6 +175,24 @@ Route::middleware('auth')->group(function () {
     Route::get('/cart/delete/gift/{id}', [CartController::class, 'deletegift'])->name('delete.gift');
     Route::post('/api/addorder', [OrderController::class, 'create'])->name('order.create');
 
-   
+
 
 });
+
+## this is post route
+
+
+
+Route::get('/posts', [BlogPostController::class, 'index'])->name('posts.index');
+
+Route::get('/posts/create', [BlogPostController::class, 'create'])->name('posts.create')->middleware('admin');
+
+Route::post('/posts', [BlogPostController::class, 'store'])->name('posts.store')->middleware('admin');
+
+Route::get('/posts/{id}', [BlogPostController::class, 'show'])->name('posts.show');
+
+Route::get('/posts/{id}/edit', [BlogPostController::class, 'edit'])->name('posts.edit')->middleware('admin');
+
+Route::put('/posts/{id}', [BlogPostController::class, 'update'])->name('posts.update')->middleware('admin');
+
+Route::delete('/posts/{id}', [BlogPostController::class, 'destroy'])->name('posts.destroy')->middleware('admin');
